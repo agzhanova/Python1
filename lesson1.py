9cb2e55b088c52c1099a89c6721edd1649e8e9d7
agzhanova <agjanova17@gmail.com>
2025-11-10 21:42:00 +0500

    Первое задание

---
+++ lesson1.py
@@ -0,0 +1,75 @@
+# print('Hello world')
+
+# num1 = int(input("Введите первое число:  "))
+# num2 = int(input("Введите второе число:  "))
+
+# print("Сумма:", num1 + num2)
+# print("Разность:", num1 - num2)
+# print("Произведение:", num1 * num2)
+# print("Деление:", num1 / num2)
+
+
+
+# number = int(input("Введите число:"))
+# divisor = int(input("Введите делитель:"))
+
+# remainder = number % divisor
+# print("Остаток от деления:", remainder)
+
+
+
+# base = int(input("Введите основание: "))
+# power = int(input("Введите степень: "))
+
+# print("Степень числа: ", base ** power)
+
+# Задача 4 
+# num1 = int(input("Введите первое число:  "))
+# num2 = int(input("Введите второе число:  "))
+# if num1>num2:
+#  print("цифра больше",num1)
+# else:
+#  print("цифра больше",num2)
+
+
+# Задача 5
+# first_name = input("Введите имя: ")
+# last_name = input("Введите фамилию: ")
+
+# print(first_name + last_name)
+
+
+# Задача 6
+# age = int(input("Введите ваш возраст: "))
+
+# a=age + 5
+# b=age + 10
+# c=age + 20
+# print("Через 5 лет Вам будет:", a)
+# print("Через 10 лет Вам будет:", b)
+# print("Через 20 лет Вам будет:", c)
+
+
+# Задача 7
+# kzt = int(input("Введите сумму в тенге: "))
+ 
+# x=kzt / 500
+# print("В долларах:", x)
+
+
+# Задача 9
+# length = int(input("Введите длину: "))
+# width = int(input("Введите ширину: "))
+
+# S=(length + width)*2
+# C= length * width
+# print("Периметр прямоугольника:", S)
+# print("Площадь прямоугольника:", C)
+
+
+age = int(input("Введите ваш возраст: "))
+if age>+18:
+    print("Можно голосовать")
+else:
+    print("Вам еще рано голосовать") 
+ 
